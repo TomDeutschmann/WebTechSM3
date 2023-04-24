58ad23cb32d6f03a6af548b5e91a90eab17409c5
Tom_Deutschmann <Tom.Deutschmann@Seriennummer.de>
2023-04-24 15:52:23 +0200

    Meilensteion 1

--- src/main/java/de/berlin/htw/webtech/demo/WebTechDemoApplication.java
+++ src/main/java/de/berlin/htw/webtech/demo/WebTechDemoApplication.java
@@ -2,6 +2,10 @@ package de.berlin.htw.webtech.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.concurrent.atomic.AtomicLong;
 
 @SpringBootApplication
 public class WebTechDemoApplication {
@@ -9,5 +13,12 @@ public class WebTechDemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(WebTechDemoApplication.class, args);
 	}
+
+	public record Greeting(long id, String content) { }
+
+	private static final String template = "Hello, %s!";
+	private final AtomicLong counter = new AtomicLong();
+
+
 
 }
